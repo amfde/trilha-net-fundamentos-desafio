1c2159cbf52ccf8a650532117b5677b17883afcb
amfde <amfservicos@hotmail.com>
2023-09-08 20:00:48 -0300

    Update

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -4,7 +4,6 @@ namespace DesafioFundamentos.Models
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
